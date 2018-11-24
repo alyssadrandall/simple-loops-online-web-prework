83ca9bfb5ba362c249697d03ad13ea08cc9532c0
Alyssa Kennedy <alyssadrandall@gmail.com>
2018-11-24 19:09:23 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -2,8 +2,9 @@ def loop_iterator(number_of_times)
   loops do
     number_of_times += 1
     puts "Welcome to Flatiron School's Web Development Course!"
-    
     break
+  end
+end
 
   # The integer is the number of times the loops should puts out the phrase
   # How can we make sure the loop breaks once it has puts out the phrase the 
